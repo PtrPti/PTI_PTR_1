b923a1782d310682e11afa2e9577f36f5495a515
Beatriz Gonçalves <filipa.isidro@gmail.com>
2020-03-12 16:44:30 +0000

    3 tentativa

---
+++ LARAVEL/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <img src=https://sigzine.com/wp-content/uploads/2019/10/Ciencias_Logo_Azul-01.png class="float-left">
+        </div>
+    </div>
+</div>
+@endsection
