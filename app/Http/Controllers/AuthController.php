cc04ec8100a12202ce1e5f1624232e90ccd5ce9d
cardoso20 <guicar97@hotmail.com>
2020-03-21 23:23:36 +0000

    alteração do readme

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -133,7 +133,6 @@ class AuthController extends Controller
                     join('departamentos', 'cursos.departamento_id', '=', 'departamentos.id')->
                     join('cadeiras', 'cursos_cadeiras.cadeira_id', '=', 'cadeiras.id')->
                     where('departamento_id', $request->departamento_id)->orderBy('cadeiras.nome')->get();
-            error_log($cadeiras);
             foreach ($cadeiras as $cadeira) {
                 $html .= '<option value="'.$cadeira->cadeira_id.'">'.$cadeira->nome.'</option>';
             }
--- resources/views/docente/disciplinasDocente.blade.php
+++ resources/views/docente/disciplinasDocente.blade.php
@@ -1,5 +1,9 @@
 <div id="disciplinas">
     @foreach($disciplinas as $disciplina)
-        <div class="disciplina">{{$disciplina->nome}}</div>
+        <div class="disciplina">
+            Nome: {{$disciplina->nome}}<br>
+            Código: {{$disciplina->cod_cadeiras}}<br>
+            Ano: {{$disciplina->ano}}
+        </div>
     @endForeach
 </div>
