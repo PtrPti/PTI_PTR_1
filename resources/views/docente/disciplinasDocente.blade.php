f653f38b45d98b7a56b696c4f64a245be739a6b8
cardoso20 <guicar97@hotmail.com>
2020-03-23 17:26:05 +0000

    style disicplinas prof

--- resources/views/docente/disciplinasDocente.blade.php
+++ resources/views/docente/disciplinasDocente.blade.php
@@ -1,11 +1,20 @@
 <div id="disciplinas">
     @foreach($disciplinas as $disciplina)
         <div class="disciplina">
-            Nome: {{$disciplina->nome}} <br/>
-            Código: {{$disciplina->cod_cadeiras}} <br/>
-            Ano: {{$disciplina->ano}} <br/>
+            <p>{{$disciplina->nome}} ({{$disciplina->cod_cadeiras}})</p>
+            <p>{{$disciplina->ano}}º ano </p>
+            <a href="#">Entrar »</a>
         </div>
     @endForeach
-    
-    <a>
+
+    @if (sizeof($disciplinas) % 4 == 1)
+        <div class="emptyDiv"></div>
+        <div class="emptyDiv"></div>
+        <div class="emptyDiv"></div>
+    @elseif (sizeof($disciplinas) % 4 == 2)
+        <div class="emptyDiv"></div>
+        <div class="emptyDiv"></div>
+    @elseif (sizeof($disciplinas) % 4 == 3)
+        <div class="emptyDiv"></div>
+    @endif
 </div>
