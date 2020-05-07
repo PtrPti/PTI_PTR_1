02f004743479bf7170ca7605129be120b00911b5
cardoso20 <guicar97@hotmail.com>
2020-05-07 23:11:34 +0100

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,16 +18,13 @@
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
-                
             }
 
             body {
-                background-image: url('/images/arrows.png');
-                background-size:70%;
+                
+                background-color:#acc;
                 background-repeat:no-repeat;
                 background-position-y:bottom;
-                
-                
             }
 
             .full-height {
@@ -37,7 +34,7 @@
             .flex-center {
                 align-items: center;
                 display: flex;
-                justify-content: center;
+                justify-content: center;                
             }
 
             .position-ref {
@@ -51,8 +48,7 @@
             }
 
             .content {
-                text-align: center;
-                
+                text-align: center;                
             }
 
             .title {
@@ -61,8 +57,8 @@
 
             .links > a {
                 color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
+                padding: 0 35px;
+                font-size: 13px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
@@ -73,36 +69,82 @@
                 margin-bottom: 30px;
             }
 
-            
-
             h3{
                 margin-top: -37px
             }
+
+            p{
+                font-size: 27px;
+                position:absolute;
+                left:670px;
+                top:350px;               
+            }
+
+            mark{
+                background-color: #e6e16c;                
+            } 
+
+            .image_logo{
+                position:absolute;
+                left:570px;
+                top:220px;
+                z-index:9999;
+            }
+
+            .image_back{
+                position:absolute;
+                bottom:5px;
+                left:0px;
+            }
+
+            @media only screen and (max-width: 768px) {
+                /* For mobile phones: */
+                [class*="image_logo"] {
+                width: 35%;
+                position:fixed;
+                margin-left:-400px;
+                align-content:center;
+                top: 135px;
+                }
+
+                [class*="image_back"] {
+                    width: 600px;
+                    position: absolute;
+                    bottom: auto;
+                }
+            }
         </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+    <body>       
+        <div class="flex-center position-ref full-height ">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    @if (Auth::check())
+                @if(Auth::user() != null)
+                    @if (Auth::user()->isAluno())
                         <a href="{{ route('homeAluno') }}">Home</a>
-                        <a href="{{ route('homeDocente') }}">docenteHome</a>
+                    @elseif (Auth::user()->isProfessor())
+                        <a href="{{ route('homeDocente') }}">Home</a>
                     @else
                         <a href="{{ url('/login') }}">Iniciar Sessão</a>
                         <a href="{{ url('/registar') }}">Registo</a>
-                    @endif
+                    @endif                  
+                @else
+                    <a href="{{ url('/login') }}">Iniciar Sessão</a>
+                    <a href="{{ url('/registar') }}">Registo</a>
+                @endif
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    <img src="{{ asset('images/big_logo.png') }}" width=25% class="image_logo">
-                </div>
+                    <img src="{{ asset('images/big_logo.png') }}" width=27% class="image_logo">
+                    <img src="{{ asset('images/group.svg') }}" width=57% class="image_back">                        
+                </div>                
 
                 <div class="links">
-                     <p>We Group, We grow</p> 
+                    <p>We Group, We grow</p>
                 </div>
-            </div>
-        </div>
+            </div>            
+        </div>        
     </body>
 </html>
