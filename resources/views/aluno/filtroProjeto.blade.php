05cb09aa03e074b0e9017386bff4ef427c98d7c1
RaquelDinis <raqueld22@hotmail.com>
2020-06-02 01:36:25 +0100

    coracaofavfeitinho

--- resources/views/aluno/filtroProjeto.blade.php
+++ resources/views/aluno/filtroProjeto.blade.php
@@ -4,18 +4,19 @@
             <p>Não está inscrito em nenhum projeto/grupo</p>
         @else
             @foreach ($projetos as $proj)
-               
-                <a href="{{ route('pagProjeto', ['id' => $proj->id]) }}">
+               <div class="grupo">
+                
                 @if($proj->favorito == 0)
                     <img onclick="changeVal(1, <?php echo $proj->usersGrupos_id ?>)" src="{{ asset('images/favorito1.png') }}" class="grupo_favorito" id="imagem_favorita">
                 @else
                     <img onclick="changeVal(0, <?php echo $proj->usersGrupos_id ?>)" src="{{ asset('images/favorito2.png') }}" class="grupo_favorito" id="imagem_favorita">
                 @endif
-                    <div class="grupo">
+                <a href="{{ route('pagProjeto', ['id' => $proj->id]) }}">
                         {{$proj->projeto}} | Grupo Nº{{$proj->numero}}<br>
                         <small>{{$proj->cadeiras}}</small>
-                    </div>
-                </a>
+                    
+                    </a>
+                </div>
             @endforeach
         @endif
     @else
